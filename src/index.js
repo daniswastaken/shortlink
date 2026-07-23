db83ad7fcfb4d63ba7a7b0dcbc50fc300153e773
daniswastaken <daniswastaken@gmail.com>
2026-07-23 09:07:06 +0700

    feat: vertical and horizontal center UI

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+const LINK = window.location.href
+
 document.getElementById('submit_link_button').addEventListener('click', function() {
     var value = document.getElementById('the_link').value;
-    prompt(value)
+    prompt(LINK)
 });  
