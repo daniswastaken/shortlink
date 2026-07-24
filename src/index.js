903d9d5a2bc7bfa670709009542878a63773c8c8
daniswastaken <daniswastaken@gmail.com>
2026-07-24 13:34:52 +0700

    feat: localhost db

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+import sqlite3 from "sqlite3";
+
+import { execute } from "./sql/sql.js";
+
+const main = async () => {
+  const db = new sqlite3.Database("shorten.db");
+  const sql = `INSERT INTO db(o_link, s_link) VALUES(?, ?)`;
+  try {
+    await execute(db, sql, ["iPhone", 899.99]);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    db.close();
+  }
+};
+
+main();
+
+/*
 // Function for pushing the link into DB
 function pushLink() {
     
@@ -7,3 +26,4 @@ document.getElementById('submit_link_button').addEventListener('click', function
     var value = document.getElementById('the_link').value;
     prompt(value)
 });
+*/
