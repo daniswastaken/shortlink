8f8ffd066683f8b8d930abd0f4c4c4639b261c99
daniswastaken <daniswastaken@gmail.com>
2026-07-24 09:11:40 +0700

    feat: refactor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-const LINK = window.location.href
+// Function for pushing the link into DB
+function pushLink() {
+    
+}
 
 document.getElementById('submit_link_button').addEventListener('click', function() {
     var value = document.getElementById('the_link').value;
-    prompt(LINK)
-});  
+    prompt(value)
+});
