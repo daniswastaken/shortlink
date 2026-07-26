08d3e987785aa4ed5d227f0c8a4f88bcc6bbb910
hyperz111 <firzeroun11@gmail.com>
2026-07-26 17:02:06 +0700

    chore: add in-memory database (Map)

--- src/lib/database.js
+++ src/lib/database.js
@@ -2,6 +2,12 @@ import { generateId, deleteId } from "./id.js";
 
 const m = new Map();
 
-export function addLink(link) {}
+export function addLink(link) {
+	const id = generateId();
+	m.set(id, link);
+	return id;
+}
 
-export function getLink(id) {}
+export function getLink(id) {
+	return m.get(id);
+}
