8f0e8b16f85c992470dd985b8de7980044888f05
daniswastaken <daniswastaken@gmail.com>
2026-07-27 18:50:56 +0700

    feat: function to retrieve what hours the link was created

--- src/lib/pushlink.js
+++ src/lib/pushlink.js
@@ -3,11 +3,16 @@ import sqlite3 from "sqlite3";
 import { execute } from "./sql.js";
 
 // Function for pushing the link into DB
-export async function pushLink(o_url, time) {
+export async function pushLink(o_url) {
+	let time = new Date();
+	let curHour = time.getHours();
+	console.log(curHour);
+
 	const db = new sqlite3.Database("shorten.db");
 	const sql = `INSERT INTO db(s_link, o_link, time) VALUES(?, ?, ?)`;
 	try {
-		await execute(db, sql, ['aaa', o_url, time]);
+		// ID, original url, hour when the push happen
+		await execute(db, sql, ['aaa', o_url, curHour]);
 	} catch (err) {
 		console.log(err);
 	} finally {
