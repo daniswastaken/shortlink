c3a6a8f436c2d169b7c55cbfc869261a86b141ff
daniswastaken <daniswastaken@gmail.com>
2026-07-27 18:29:08 +0700

    feat: db rewrite

--- src/lib/pushlink.js
+++ src/lib/pushlink.js
@@ -5,9 +5,9 @@ import { execute } from "./sql.js";
 // Function for pushing the link into DB
 export async function pushLink(o_url, time) {
 	const db = new sqlite3.Database("shorten.db");
-	const sql = `INSERT INTO db(o_link, s_link, time) VALUES(?, ?, ?)`;
+	const sql = `INSERT INTO db(s_link, o_link, time) VALUES(?, ?, ?)`;
 	try {
-		await execute(db, sql, [o_url, 15, time]);
+		await execute(db, sql, ['aaa', o_url, time]);
 	} catch (err) {
 		console.log(err);
 	} finally {
--- tests/create_db.sh
+++ tests/create_db.sh
@@ -1,10 +1,6 @@
-# Source - https://stackoverflow.com/a/69123002
-# Posted by mklement0
-# Retrieved 2026-07-24, License - CC BY-SA 4.0
-
 # The commands to submit - note that each statement must be ";"-terminated.
 cmds='
-create table db (o_link, s_link, time); 
+create table db (s_link, o_link, time); 
 '
 
 # Pipe the commands to `sqlite3` while also passing the database file path.
